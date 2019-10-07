2fc24110aa2b2c6cc69cd487b2e6687ff8a0b6e8
Hugo Biarge <hbiarge@plainconcepts.com>
2019-10-07 11:16:45 +0200

    Fix principal formatted log

--- src/Acheve.Authentication.Events/ClaimsPrincipalExtensions.cs
+++ src/Acheve.Authentication.Events/ClaimsPrincipalExtensions.cs
@@ -7,8 +7,18 @@ namespace Acheve.Authentication.Events
     {
         public static string ToFormattedLog(this ClaimsPrincipal principal)
         {
+            if (principal is null)
+            {
+                return "No principal created";
+            }
+
+            // Try to identify the name of the principal
+            var name = principal.Identity.Name
+                       ?? principal.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value
+                       ?? principal.FindFirst(c => c.Type == "sub")?.Value;
             var identities = string.Join(", ", principal.Identities.Select(i => $"{i.AuthenticationType} - Authenticated: {i.IsAuthenticated}"));
-            return $"{principal.Identity.Name} - Identities: {identities})";
+
+            return $"{name} with {principal.Identities.Count()} identity(es): {identities})";
         }
     }
 }
